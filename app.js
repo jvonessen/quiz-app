635dbd39ee352444e7f64d50b37c8b207c641920
Jen Von Essen <cgajen09@gmail.com>
2017-01-12 21:17:54 -0800

    typo fix

--- app.js
+++ app.js
@@ -157,11 +157,11 @@ $('#restart-quiz').click(function(event) {
   $(".question-page section:nth-child(1)").removeClass("hidden")
 });
 
-$('.answer').on("click touchstart", ".answer", function(event) {
+$('.question-list').on("click touchstart", ".answer", function(event) {
   checkAnswer(state, this);
   $(".next-question").removeClass("hidden");
 });
 
-$(".question-list").on("click touchstart", ".next-question", function(event) {
+$(".question-list").on("click", ".next-question", function(event) {
   advanceQuestion(state, this);
 });
